93e2eb8275d44c37d288e69a3e3b062ff531f466
J-Cheol <lunaris0206@naver.com>
2023-11-06 09:45:17 +0900

    step 1 : end commend

---
+++ src/main/java/com/ll/App.java
@@ -0,0 +1,14 @@
+package com.ll;
+
+import java.util.Scanner;
+
+public class App
+{
+    public void run()
+    {
+        System.out.println("== 명언 앱 ==");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("명령) ");
+        String cmd = scanner.next();
+    }
+}
