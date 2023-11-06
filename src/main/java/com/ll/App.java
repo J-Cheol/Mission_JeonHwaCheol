cbe7d780bf6af34e45f3df171e542207e1d6fcfb
J-Cheol <lunaris0206@naver.com>
2023-11-06 14:08:25 +0900

    refactoring, fuction divide

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -6,12 +6,20 @@ import java.util.Scanner;
 
 public class App
 {
+    Scanner scanner = new Scanner(System.in);
+    List<Quotation> quotations = new ArrayList<>();
+    int id = 0;
+
+    App()
+    {
+        scanner = new Scanner(System.in);
+        id = 0;
+        quotations = new ArrayList<>();
+    }
     public void run()
     {
         System.out.println("== 명언 앱 ==");
-        Scanner scanner = new Scanner(System.in);
-        List<Quotation> quotations = new ArrayList<>();
-        int id = 0;
+
         while (true)
         {
             System.out.print("명령) ");
@@ -19,72 +27,81 @@ public class App
             if (cmd.equalsIgnoreCase("종료"))
                 return;
             if (cmd.equalsIgnoreCase("등록"))
-            {
-                scanner.nextLine();
-                System.out.print("명언 : ");
-                String quote = scanner.nextLine();
-                System.out.print("작가 : ");
-                String author = scanner.nextLine();
-                id++;
-                System.out.printf("%d번 명언이 등록되었습니다.\n", id);
-                Quotation quotation = new Quotation(id, author, quote);
-                quotations.add(quotation);
-            }
+                action_registration();
             if (cmd.equalsIgnoreCase("목록"))
-            {
-                System.out.println("번호 / 작가 / 명언");
-                System.out.println("----------------------");
-                for (int i = quotations.size() - 1; i >= 0; i--)
-                {
-                    Quotation quotation = quotations.get(i);
-                    if (quotations.get(i).author != null || quotations.get(i).catalog != null)
-                        System.out.printf("%d / %s / %s\n", quotation.id, quotation.author, quotation.catalog);
-                }
-            }
+                action_list();
             if (cmd.startsWith("삭제?id="))
-            {
-                String[] parts = cmd.split("\\?");
-                String find_del = parts[1];
-                String[] find_del_part = find_del.split("=");
-                try
-                {
-                    int del_id = Integer.parseInt(find_del_part[1]);
-                    if (del_id > 0 && del_id <= quotations.size())
-                    {
-                        Quotation check = quotations.get(del_id - 1);
-                        if (del_id == check.id && quotations.get(del_id - 1).author != null)
-                        {
-                            System.out.printf("%d번 명언이 삭제되었습니다.\n", del_id);
-                            quotations.get(del_id - 1).id = del_id;
-                            quotations.get(del_id - 1).author = null;
-                            quotations.get(del_id - 1).catalog = null;
-                        } else
-                        {
-                            System.out.printf("%d번 명언이 존재하지 않습니다.\n", del_id);
-                        }
-                    }
-                } catch(NumberFormatException e){}
-            }
+                action_delete(cmd);
             if (cmd.startsWith("수정?id="))
-            {
-                String[] parts = cmd.split("\\?");
-                String find_edit = parts[1];
-                String[] find_edit_part = find_edit.split("=");
-                try
-                {
-                    int edit_id = Integer.parseInt(find_edit_part[1]);
-                    if (edit_id > 0 && edit_id <= quotations.size())
-                    {
-                        System.out.println("명언(기존) : " + quotations.get(edit_id - 1).catalog);
-                        scanner.nextLine();
-                        System.out.print("명언 : ");
-                        quotations.get(edit_id - 1).catalog = scanner.nextLine();
-                        System.out.println("작가(기존) : " + quotations.get(edit_id - 1).author);
-                        System.out.print("작가 : ");
-                        quotations.get(edit_id - 1).author = scanner.nextLine();
-                    }
-                }catch(NumberFormatException e){}
-            }
+                action_edit(cmd);
         }
     }
+    void action_registration()
+    {
+        scanner.nextLine();
+        System.out.print("명언 : ");
+        String quote = scanner.nextLine();
+        System.out.print("작가 : ");
+        String author = scanner.nextLine();
+        id++;
+        System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+        Quotation quotation = new Quotation(id, author, quote);
+        quotations.add(quotation);
+    }
+    void action_list()
+    {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("----------------------");
+        for (int i = quotations.size() - 1; i >= 0; i--)
+        {
+            Quotation quotation = quotations.get(i);
+            if (quotations.get(i).author != null || quotations.get(i).catalog != null)
+                System.out.printf("%d / %s / %s\n", quotation.id, quotation.author, quotation.catalog);
+        }
+    }
+    void action_delete(String cmd)
+    {
+        String[] parts = cmd.split("\\?");
+        String find_del = parts[1];
+        String[] find_del_part = find_del.split("=");
+        try
+        {
+            int del_id = Integer.parseInt(find_del_part[1]);
+            if (del_id > 0 && del_id <= quotations.size())
+            {
+                Quotation check = quotations.get(del_id - 1);
+                if (del_id == check.id && quotations.get(del_id - 1).author != null)
+                {
+                    System.out.printf("%d번 명언이 삭제되었습니다.\n", del_id);
+                    quotations.get(del_id - 1).id = del_id;
+                    quotations.get(del_id - 1).author = null;
+                    quotations.get(del_id - 1).catalog = null;
+                } else
+                {
+                    System.out.printf("%d번 명언이 존재하지 않습니다.\n", del_id);
+                }
+            }
+        } catch(NumberFormatException e){}
+    }
+    void action_edit(String cmd)
+    {
+        String[] parts = cmd.split("\\?");
+        String find_edit = parts[1];
+        String[] find_edit_part = find_edit.split("=");
+        try
+        {
+            int edit_id = Integer.parseInt(find_edit_part[1]);
+            if (edit_id > 0 && edit_id <= quotations.size())
+            {
+                System.out.println("명언(기존) : " + quotations.get(edit_id - 1).catalog);
+                scanner.nextLine();
+                System.out.print("명언 : ");
+                quotations.get(edit_id - 1).catalog = scanner.nextLine();
+                System.out.println("작가(기존) : " + quotations.get(edit_id - 1).author);
+                System.out.print("작가 : ");
+                quotations.get(edit_id - 1).author = scanner.nextLine();
+            }
+        }catch(NumberFormatException e){}
+    }
 }
+
--- src/main/java/com/ll/Quotation.java
+++ src/main/java/com/ll/Quotation.java
@@ -5,7 +5,7 @@ public class Quotation
     int id;
     String author;
     String catalog;
-    Quotation(int id, String author, String catalog)
+    public Quotation(int id, String author, String catalog)
     {
         this.id = id;
         this.author = author;
