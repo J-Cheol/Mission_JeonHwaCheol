cf7a17d257547f2d106710889cd11fec6337553a
J-Cheol <lunaris0206@naver.com>
2023-11-06 10:01:27 +0900

    step 2 : register function add

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -8,7 +8,20 @@ public class App
     {
         System.out.println("== 명언 앱 ==");
         Scanner scanner = new Scanner(System.in);
-        System.out.print("명령) ");
-        String cmd = scanner.next();
+        while (true)
+        {
+            System.out.print("명령) ");
+            String cmd = scanner.next();
+            if (cmd.equalsIgnoreCase("종료"))
+                return;
+            if (cmd.equalsIgnoreCase("등록"))
+            {
+                scanner.nextLine();
+                System.out.print("명언 : ");
+                scanner.nextLine();
+                System.out.print("작가 : ");
+                scanner.nextLine();
+            }
+        }
     }
 }
