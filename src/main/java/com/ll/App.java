f5ace8cfcc3a85788325b099bcfbe1bebe3699bc
J-Cheol <lunaris0206@naver.com>
2023-11-06 12:01:29 +0900

    step 7 : Delete exception handling add

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -37,7 +37,8 @@ public class App
                 for (int i = quotations.size() - 1; i >= 0; i--)
                 {
                     Quotation quotation = quotations.get(i);
-                    System.out.printf("%d / %s / %s\n", quotation.id, quotation.author, quotation.catalog);
+                    if (quotations.get(i).author != null || quotations.get(i).catalog != null)
+                        System.out.printf("%d / %s / %s\n", quotation.id, quotation.author, quotation.catalog);
                 }
             }
             if (cmd.startsWith("삭제?id="))
@@ -46,8 +47,20 @@ public class App
                 String find_del = parts[1];
                 String[] find_del_part = find_del.split("=");
                 int del_id = Integer.parseInt(find_del_part[1]);
-                quotations.remove(del_id - 1);
-                System.out.printf("%d번 명언이 삭제되었습니다.\n", del_id);
+                if (del_id > 0 && del_id <= quotations.size())
+                {
+                    Quotation check = quotations.get(del_id - 1);
+                    if (del_id == check.id && quotations.get(del_id - 1).author != null)
+                    {
+                        System.out.printf("%d번 명언이 삭제되었습니다.\n", del_id);
+                        quotations.get(del_id - 1).id = del_id;
+                        quotations.get(del_id - 1).author = null;
+                        quotations.get(del_id - 1).catalog = null;
+                    } else
+                    {
+                        System.out.printf("%d번 명언이 존재하지 않습니다.\n", del_id);
+                    }
+                }
             }
         }
     }
